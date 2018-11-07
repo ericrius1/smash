1c75165d93bc455be8f6e7edf8f15597529a76a3
ericrius1 <ericrius1@gmail.com>
2018-11-06 21:30:24 -0800

    Updates

---
+++ precache-manifest.7732b721d8d5807343ea458eda662127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smash/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "74d158e0cabcd348b9b1",
+    "url": "/smash/static/js/runtime~main.74d158e0.js"
+  },
+  {
+    "revision": "a0c8b02b756e7801c670",
+    "url": "/smash/static/js/main.a0c8b02b.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/smash/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "a0c8b02b756e7801c670",
+    "url": "/smash/static/css/main.bf07e8ff.chunk.css"
+  },
+  {
+    "revision": "de4a3c16965ec274b3205ce8e461444c",
+    "url": "/smash/index.html"
+  }
+];
